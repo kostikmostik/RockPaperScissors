40e40051de1f55d39cd9a676b2a3be9d9fcf0e96
kostikmostik <kosta.yus@gmail.com>
2022-01-25 22:13:33 -0800

    Added basic DOM manipulation to the UI

--- main.js
+++ main.js
@@ -1,27 +1,32 @@
 
 let options = ['rock', 'paper', 'scissors'];
 
-let playerScore;
-let computerScore;
+let playerScore = 0;
+let computerScore = 0;
 
 let playerSelection;
 let computerSelection;
 
-game();
+const buttonNodeList = document.querySelectorAll("button");
+const gameResult = document.querySelector('#result');
+const content = document.createElement('p');
+content.classList.add('content');
 
-function game() //play 5 rouns and determine winner
+buttonNodeList.forEach((button) => 
+
 {
-    computerScore = 0;
-    playerScore = 0;
-    for (let i = 0; i <= 4; i++)
-    {
-        playerSelection = prompt("Rock, paper, or scissors?");
-        computerSelection = computerPlay();
-        console.log(`Player: ${playerSelection}; Computer: ${computerSelection}`);
-        playRound (playerSelection, computerSelection)
-    }
-    console.log(`Computer score ${computerScore}, plyer score ${playerScore}`);
-    alert(`Computer score ${computerScore}, plyer score ${playerScore}`);
+    button.addEventListener('click', () => {game(button.id);});
+
+});
+
+function game(playerSelection)
+{
+    computerSelection = computerPlay();
+    console.log(`Player: ${playerSelection}; Computer: ${computerSelection}`);
+    content.textContent = `Player: ${playerSelection}; Computer: ${computerSelection}`;
+    container.appendChild(content);
+    playRound (playerSelection, computerSelection);
+
 }
 
 function computerPlay() //generate computer selection
@@ -38,18 +43,23 @@ if ((playerSelection === 'rock' && computerSelection === "scissors") ||
    (playerSelection === 'scissors' && computerSelection === "paper")){
     playerScore++;
     console.log(`You won, ${playerSelection} beats ${computerSelection}`);
+    content.textContent = `You won, ${playerSelection} beats ${computerSelection}`;
+    container.appendChild(content);
 }
 
 if ((playerSelection === 'rock' && computerSelection === "paper") ||
    (playerSelection === 'paper' && computerSelection === "scissors") ||
    (playerSelection === 'scissors' && computerSelection === "rock")){
     computerScore++;
-    console.log(`You lost, ${computerSelection} beats ${playerSelection}`);
+    content.textContent = `You lost, ${computerSelection} beats ${playerSelection}`;
+    container.appendChild(content);
 }
 
 if ((playerSelection === 'rock' && computerSelection === "rock") ||
    (playerSelection === 'paper' && computerSelection === "paper") ||
    (playerSelection === 'scissors' && computerSelection === "scissors")){
-    console.log(`Draw..${computerSelection} VS ${playerSelection}`);
+    content.textContent = `Draw..${computerSelection} VS ${playerSelection}`;
+    container.appendChild(content);
 }
 }
+
